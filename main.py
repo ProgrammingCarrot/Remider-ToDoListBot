267abdc05a5e26cc75b77f9b95d84a712b008c92
webber <carrotbloxer@gmail.com>
2025-05-24 04:08:41 +0800

    JSON retry

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask,request,abort
-from linebot import(
-    LineBotApi,WebhookHandler
+import os
+import sys
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
 )
-from linebot.exceptions import(
+from linebot.v3.exceptions import (
     InvalidSignatureError
 )
 from linebot.v3.messaging import (
@@ -45,16 +48,17 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message(
+        line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
-                event.reply_token,
-                TextMessage(text=event.message.text)
-                )
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
             )
+        )
 
 if __name__ == "__main__":
     app.run()
