b5f6af83c92aa95b796a1dc320ecad98ae26e62e
webber <carrotbloxer@gmail.com>
2025-05-26 02:03:28 +0800

    rewrite webhook and api

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
-import os
-import sys
-from flask import Flask, request, abort
-
-from linebot.v3 import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
-
 import json
+import base64
+import hashlib
+import hmac
+import requests
+from flask import Flask,request,abort
 
 app = Flask(__name__)
 
-with open("./assets/keys.json", "r") as file:
+with open("./assets/keys.json") as file:
     keys = json.load(file)
 
-configuration = Configuration(access_token=keys["access_token"])
-handler = WebhookHandler(keys["chennel_secret"])
+channel_secret = keys["channel_secret"]
+chennel_access_token = keys["access_token"]
 
-@app.route("/callback", methods=['POST'])
+@app.route("/",methods = ['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    #獲得請求的簽名
+    signature = request.headers.get('X-Line-Signature')
 
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    request = json.loads(body)
+    #驗證本地簽名和請求簽名是否一致
     try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        local_signature = base64.b64encode(hmac.new(channel_secret.encode('utf-8'),body.encode('utf-8'), hashlib.sha256).digest()).decode()
+        if signature != local_signature:
+            abort(400)
+    except Exception as exception:
+        app.logger.error(f"Signature verification error: {exception}")
+        abort(500)  
+
+    try:
+        json_data = json.loads(body)
+        reply_message(json_data)   
+
+        return 'OK'
+    
+    except json.JSONDecodeError:
+        app.logger.error("無法解析 JSON 請求主體")
         abort(400)
+    
+    except Exception as exception:
+        app.logger.error(f"處理事件時發生錯誤: {exception}")
+        abort(500)  
 
-    return 'OK'
+def reply_message(request):
+    headers = {'Authorization':'Bearer ' + chennel_access_token,'Content-Type':keys['content_type']}
+    if request['events']['type'] == "message":
+        message = request['events']['messgae']
+        reply_token = request['events']['replyToken']
+        reply_message = {
+            "replyToken":reply_token,
+            "messages":
+            {
+                "type":"text",
+                "text":message
+            }
+        }
+    else:
+        return 0
+    requests.post('https://api.line.me/v2/bot/message/reply',headers=headers,data=json.dumps(reply_message))
 
+    
 
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
 
 if __name__ == "__main__":
     app.run()
